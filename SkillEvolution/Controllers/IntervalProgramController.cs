6563de63ca0b946790aa6846f52d6fbbbf6f6bc2
Chirag Patel <chiragpatel.cra@gmail.com>
2024-04-17 14:58:01 +0530

    Merge Interval Progream

---
+++ SkillEvolution/Controllers/IntervalProgramController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SkillEvolution.Models;
+namespace SkillEvolution.Controllers
+{
+    public class IntervalProgramController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IList<Interval> MergeOverlapping(IList<Interval> intervals)
+        {
+            var outVal = new SortedSet<Interval>(intervals, new IntervalComparer());
+            for (int i = 0; i < outVal.Count - 1; i++)
+            {
+                Interval curr = outVal.ElementAt(i);
+                Interval next = outVal.ElementAt(i + 1);
+                if (next.Start - curr.End < 1)
+                {
+                    curr.End = Math.Max(curr.End, next.End);
+                    outVal.Remove(next);
+                    i--;
+                }
+            }
+            return outVal.ToList();
+        }
+    }
+
+
+    public class IntervalComparer : IComparer<Interval>
+    {
+        public int Compare(Interval lhs, Interval rhs)
+        {
+            if (lhs.Start == rhs.Start)
+            {
+                return lhs.End.CompareTo(rhs.End);
+            }
+            return lhs.Start.CompareTo(rhs.Start);
+        }
+    }
+}
---
+++ SkillEvolution/Models/Interval.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillEvolution.Models
+{
+    public class Interval
+    {
+        public int Start;
+        public int End;
+        public Interval() { Start = 0; End = 0; }
+        public Interval(int s, int e) { Start = s; End = e; }
+    }
+}
